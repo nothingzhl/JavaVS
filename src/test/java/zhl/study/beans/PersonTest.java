ae6c0669c3f6d4f37125f2d09ddc3845565b2314
zhanghanlin <zhanghanlinhs@163.com>
2020-07-22 23:36:24 +0800

    bean相关

---
+++ src/test/java/zhl/study/beans/PersonTest.java
@@ -0,0 +1,38 @@
+package zhl.study.beans;
+
+import org.junit.jupiter.api.Test;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyEditorSupport;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeansTest {
+
+    @Test
+    void testBeanInfo() throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class, Object.class);
+        Arrays.asList(beanInfo.getPropertyDescriptors()).forEach(System.out::println);
+    }
+
+
+    @Test
+    void testPropertyEdit() throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class, Object.class);
+        Arrays.asList(beanInfo.getPropertyDescriptors()).forEach(propertyDescriptor -> {
+            if ("age".equals(propertyDescriptor.getName())) {
+                propertyDescriptor.setPropertyEditorClass(StringToIntegerPropertyEdit.class);
+            }
+        });
+    }
+
+    static class StringToIntegerPropertyEdit extends PropertyEditorSupport{
+        @Override
+        public void setAsText(String text) throws IllegalArgumentException {
+            setValue(Integer.valueOf(text));
+        }
+    }
+}
